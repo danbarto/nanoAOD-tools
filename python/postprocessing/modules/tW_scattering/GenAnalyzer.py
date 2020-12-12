ac422436e94997e2ff647737c5e35376eede36ca
Kaitlin Salyer <ksalyer@uaf-10.t2.ucsd.edu>
2020-12-12 15:24:15 -0800

    latest

--- python/postprocessing/modules/tW_scattering/GenAnalyzer.py
+++ python/postprocessing/modules/tW_scattering/GenAnalyzer.py
@@ -31,11 +31,12 @@ def hasBit(value,bit):
 
 class GenAnalyzer(Module):
 
-    def __init__(self,isW,isWExt):
+    def __init__(self,isW,isWExt,isT,isTExt,year):
         self.isW = isW
         self.isWExt = isWExt
-        print isW
-        print isWExt
+        self.isT = isT
+        self.isTExt = isTExt
+        self.year = year
         pass
 
     def beginJob(self):
@@ -196,7 +197,8 @@ class GenAnalyzer(Module):
         #MET
         met_pt  = event.MET_pt
         met_phi = event.MET_phi
-        
+        genMET_pt  = event.GenMET_pt
+
         #Particles
         GenParts    = Collection(event, "GenPart")
         GenAK8Jets  = Collection(event, "GenJetAK8")
@@ -252,6 +254,13 @@ class GenAnalyzer(Module):
                 sumPt = sumPt + nu.pt
             if self.isWExt and sumPt <200: stitch = False
             if not self.isWExt and sumPt >200: stitch = False
+        elif self.isT:
+            if self.year == 2018:
+                if self.isTExt and genMET_pt<80: stitch = False
+                if not self.isTExt and genMET_pt>80: stitch = False
+            else:
+                if self.isTExt and genMET_pt<150: stitch = False
+                if not self.isTExt and genMET_pt>150: stitch = False
 
         for jet in GenJets:
             jet.genPartWIdx = -1 # this is the reference to the W in the GenPart collection
@@ -491,4 +500,4 @@ class GenAnalyzer(Module):
 
 # define modules using the syntax 'name = lambda : constructor' to avoid having them loaded when not needed
 
-genAnalyzer = lambda  isW, isWExt : GenAnalyzer( isW=isW, isWExt=isWExt )
+genAnalyzer = lambda  isW, isWExt, isT, isTExt, year : GenAnalyzer( isW=isW, isWExt=isWExt, isT=isT, isTExt=isTExt, year=year )
